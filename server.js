d159373939113daef905ce9913e816a13fe2bc96
Moin Shaikh <7282553+moingshaikh@users.noreply.github.com>
2021-02-11 21:09:59 +0530

    add login POST route

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
 
 const mockUserData=[
 {name:'Mark'},
@@ -21,5 +24,27 @@ app.get('/users/:id',function(req,res){
 		user: req.params.id
 	})
 })
+
+app.post('/login',function(req,res){
+ 	const username=req.body.username;
+ 	const password=req.body.password;
+ 
+ 	const mockUsername="billyTheKid";
+ 	const mockPassword="superSecret";
+ 
+ 	if (username===mockUsername && password===mockPassword){
+      	res.json({
+      	 	success: true,
+      	 	message: 'password and username match!',
+      	 	token: 'encrypted token goes here'
+      	})
+ 	} else {
+      	res.json({
+      	 	success: false,
+      	 	message: 'password and username do not match'
+      	})
+ 	}
+})
+
 
 app.listen(8000,function(){console.log('server is listening')})
